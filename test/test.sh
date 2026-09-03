c7036c5f0dcc842df86720d4cb7e87c805bd3dc9
bernborgess <bernborgess@outlook.com>
2026-09-03 14:12:56 +0000

    Add png of dot graphs generation to test script

--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,7 @@ TESTS=('array' 'call12' 'i-plus-j' 'i-plus-plus' 'official' 'readif' \
     'return-compare' 'return012' 'while10' 'x14') # 'wcet'
 
 CJC_PATH=../../cangjie_compiler/output/bin/
+GENERATE_GRAPHS=false
 
 for test in "${TESTS[@]}"; do
     echo -n "Test case '$test' "
@@ -10,6 +11,14 @@ for test in "${TESTS[@]}"; do
 
     # Run the code
     $CJC_PATH/cjc code.cj -O2 -j1
+
+    # Generate png of dot graphs
+    if [ $GENERATE_GRAPHS = true ]; then
+    for graph in *.dot; do
+        # https://mywiki.wooledge.org/BashFAQ/073
+        dot -Tpng "$graph" -o "${graph%.*}.png"
+    done
+    fi
 
     # Compare the output with expected
     if diff output.txt expected.txt; then
